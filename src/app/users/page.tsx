e70706efc8cf87dff8739b9ca3545c208f2c6655
gylgyl2000 <gylgyl2000@pm.me>
2024-08-13 20:59:34 +0200

    update

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -7,12 +7,12 @@ import { Users } from "@/lib/users"
 import { useSearchParams } from 'next/navigation'
 
 const UsersPage = () => {
-    const [userProfileData, setUserProfileData] = useState<UserProfile[]>([])
+    // const [userProfileData, setUserProfileData] = useState<UserProfile[]>([])
     const searchParams = useSearchParams()
     const searchQuery = searchParams && searchParams.get("search")
 
-    useEffect(() => {
-        const handleSearch = () => {
+    // useEffect(() => {
+        // const handleSearch = () => {
             const findUser = Users.filter((user) => {
                 if (searchQuery) {
                     return (
@@ -25,12 +25,13 @@ const UsersPage = () => {
                     return true;
                 }
             });
-            setUserProfileData(findUser);
-        };
-        handleSearch();
-    }, [searchQuery]);
+            // setUserProfileData(findUser);
+        // };
+        // handleSearch();
+    // }, [searchQuery]);
 
-  const totalUsers = userProfileData.length;
+//   const totalUsers = userProfileData.length
+  const totalUsers = findUser.length
 
   return (
     <section className="h-[100vh] w-screen px-[2rem] md:px-[6rem] mt-[100px]">
@@ -39,7 +40,7 @@ const UsersPage = () => {
         <div className="mt-8">
             {totalUsers === 0 ? <p>Aucun résultat</p> : (
                 <div className="m-20 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 items-center gap-8">
-                    {userProfileData.map((user, index) => (
+                    {findUser.map((user, index) => (
                             <div key={index}>
                                 <ProfileCard
                                     name={user.identification.full_name}
